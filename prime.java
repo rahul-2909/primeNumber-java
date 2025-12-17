83730486e9c6f4db302d6cd8016918a5fb7aedf5
Rahul Rohit <rohitrahul2909@gamil.com>
2025-12-17 22:11:48 +0530

    prime number

--- prime.java
+++ prime.java
@@ -1,48 +1,66 @@
-import java.util.Scanner;
+// import java.util.Scanner;
+
+// public class prime {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         if (n == 2) {
+//             System.out.println(n + " is prime number");
+//         } else {
+//             boolean isprime = true;
+
+//             for (int i = 2; i <= n - 1; i++) {
+//                 if (n % i == 0) {
+//                     isprime = false;
+//                 }
+
+//                 if (isprime == true) {
+//                     System.out.println(n + " is prime number");
+//                 } else {
+//                     System.out.println(n + " is not prime number");
+//                 }
+
+//             }
+//         }
+
+//     }
+// }
+// // ===============================================
+
+// // if (n == 2) {
+// // System.out.println(n+ " is prime number");
+// // }else{
+// // boolean isprime = true;
+
+// // for(int i=2;i<=n-1;i++)
+// // {
+// // if (n % i == 0) {
+// // isprime=false;
+// // }
+// // }
+// // if (isprime == true) {
+// // System.out.println("n is prime");
+// // }
+// // else{
+// // System.out.println("n is not prime");
+// // }
+// // }
+
 
 public class prime {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        if (n == 2) {
-            System.out.println(n + " is prime number");
-        } else {
-            boolean isprime = true;
-
-            for (int i = 2; i <= n - 1; i++) {
-                if (n % i == 0) {
-                    isprime = false;
-                }
-
-                if (isprime == true) {
-                    System.out.println(n + " is prime number");
-                } else {
-                    System.out.println(n + " is not prime number");
-                }
-
+    public static boolean isprime(int n){
+        boolean isprime = true;
+        for(int i=2;i<=n-1;i++){
+            if (n % i == 0) {
+                isprime = false;
+                break;
             }
         }
-
+        return isprime;
+    }
+    public static void main(String[] args) {
+        System.out.println(isprime(5));
     }
 }
-// ===============================================
-
-// if (n == 2) {
-// System.out.println(n+ " is prime number");
-// }else{
-// boolean isprime = true;
-
-// for(int i=2;i<=n-1;i++)
-// {
-// if (n % i == 0) {
-// isprime=false;
-// }
-// }
-// if (isprime == true) {
-// System.out.println("n is prime");
-// }
-// else{
-// System.out.println("n is not prime");
-// }
-// }
